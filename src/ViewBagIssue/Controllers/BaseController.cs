9dfe7ffa3adcc0ec3aac92911ba1770d67eb2dc3
Todd Culbertson <tculbertson@priorityonetv.com>
2017-01-23 15:16:53 -0600

    first commit

---
+++ src/ViewBagIssue/Controllers/BaseController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ViewBagIssue.Controllers
+{
+    public class BaseController : Controller
+    {
+        public BaseController()
+        {
+            ViewBag.Title = "Foo";
+        }
+    }
+}
---
+++ src/ViewBagIssue/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ViewBagIssue.Controllers
+{
+    public class HomeController : BaseController
+    {
+        public IActionResult Index()
+        {
+            ViewBag.PageName = "Bar";
+            return View();
+        }        
+    }
+}
